8c6feccc2d489835b346bd5d64ae03e4934c2d85
Fadhlallah Almohammed <2230006097@iau.edu.sa>
2024-02-16 05:09:04 +0300

    Fadhlallah Almohammed - Task 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = myObj.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = myObj.nextInt();
+
+        System.out.println();
+
+        if (num1 > num2) {
+            System.out.println("The first number (" + num1 + ") is greater than the second number");
+        }
+        else if (num1 < num2) {
+            System.out.println("The second number (" + num2 + ") is greater than the first number");
+        }
+        else {
+            System.out.println("They are equal!");
+        }
     }
 }
