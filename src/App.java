165d7edcd5ad9a4c99eed883ef434cf7db29d654
Ahmed-Alawlaqi <ahm.m.awlaqi@gmail.com>
2024-02-16 04:01:35 +0300

    First commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2);
+        } else if (num1 < num2) {
+            System.out.println(num1 + " is less than " + num2);
+        } else {
+            System.out.println(num1 + " is equal to " + num2);
+            
+        }
+        
+
+        //POV: when I see java code: "Run with your life bro"
+        System.out.println("    O   ");
+        System.out.println("   /|\\/ ");
+        System.out.println("   \\/\\   ");
+        System.out.println("      /  ");
+
+        System.out.println("    O   ");
+        System.out.println("   /|\\/ ");
+        System.out.println("   /\\   ");
+        System.out.println("  /  |  ");
+        
+        sc.close();
+
     }
 }
