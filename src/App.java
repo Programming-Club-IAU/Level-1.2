8ab870de51ff65880099804ad0bf20dec932eda1
Haya <145580738+H4ya@users.noreply.github.com>
2024-02-17 18:47:44 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        if ( x > y )
+          System.out.println(x +" is bigger than " + y) ;
+        else if (y > x)
+          System.out.println(y +" is bigger than "+ x);
+        else 
+          System.out.println("Both x and y are equal");
+        
+
+
+      System.out.println();
     }
 }
+
