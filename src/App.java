b2880742c9961c94bed36c49b77e682d26f24c15
Haya <145580738+H4ya@users.noreply.github.com>
2024-02-20 10:50:57 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,22 +1,36 @@
 import java.util.Scanner;
+/*
+        System.out.println("(\\__/)");
+        System.out.println("( 0.0)");
+        System.out.println("/    >");
+        System.out.println("v___v");
 
+*/
 public class App {
     public static void main(String[] args){
         
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of x: ");
         double x = sc.nextDouble();
+        System.out.println("Enter the value of y: ");
         double y = sc.nextDouble();
+        if ( x > y ){
+          System.out.println("(\\__/)");
+          System.out.println("( 0.0)");
+          System.out.println("/    > "+x +"(x) is bigger than " + y+"(y)") ;
+        }
+        else if (y > x){
+          System.out.println("(\\__/)");
+          System.out.println("( 0.0)");
+          System.out.println("/    > "+y +"(y) is bigger than "+ x+"(x)");
+        }
+        else{
+          System.out.println("(\\__/)");
+          System.out.println("( -.-)");
+          System.out.println("/    > Both x and y are equal..");
 
-        if ( x > y )
-          System.out.println(x +" is bigger than " + y) ;
-        else if (y > x)
-          System.out.println(y +" is bigger than "+ x);
-        else 
-          System.out.println("Both x and y are equal");
-        
+        }
 
-
-      System.out.println();
     }
 }
 
