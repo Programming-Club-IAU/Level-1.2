fb3b2e70c40b010fe66ec92f8f88d544f4248e9c
Mohmmad-IAU <2240005057@iau.edu.sa>
2024-02-22 18:15:05 +0300

    New

---
+++ src/CompareNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the first number:");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Please enter the second number:");
+        int secondNumber = scanner.nextInt();
+        String art =   "K   K   SSSS  A   A\n" +
+                       "K  K   S      A A\n" +
+                       "KKK    SSS   AAAAA\n" +
+                       "K  K      S  A   A\n" +
+                       "K   K  SSSS  A   A\n";
+        if (firstNumber > secondNumber) {
+            System.out.println(art + "The first number (" + firstNumber + ") is greater than the second number (" + secondNumber + ").");
+        } else if (secondNumber > firstNumber) {
+            System.out.println(art + "The second number (" + secondNumber + ") is greater than the first number (" + firstNumber + ").");
+        } else {
+            System.out.println(art + "The numbers are equal.");
+        }
+    }
+}
+
