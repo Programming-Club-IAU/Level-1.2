dedfa64bd9d19fe805dbc3e39ce5ef93d213071e
Ruqayah Mousa <ruqayah.almousa@gmail.com>
2024-02-24 12:42:29 +0300

    <maximmum numbers project>

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two integer numbers:");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        if (num1 > num2) {
+            System.out.println("The first number is the greatest number:" + num1);
+
+        } else if (num1 < num2) {
+            System.out.println("The second number is the greatest number:" + num2);
+
+        } else {
+            System.out.println("The first number " + num1 + " is equal to the second number " + num2);
+        }
     }
 }
